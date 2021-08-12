99a10157da2b680c425bf3eb53d78beccbe65ecb
Sancho Godinho <73981314+sancho1952007@users.noreply.github.com>
2021-08-12 20:17:00 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 function message(text){
     var div=document.createElement('div')
+    div.style.position='fixed';
     div.style.animation='msg 1s linear';
     div.setAttribute('id', 'div_Msg')
     div.style.backgroundColor = 'lightblue';
-    div.style.position='absolute';
     div.style.display='flex';
     div.innerHTML='&nbsp;<center><h3 style="cursor: default;" class="msg-text">'+text+'</h3></center><button onclick="closeMsg()" class="msg-text" style="cursor: pointer; background-color: transparent; border: 0px; outline: none; color: grey;">&nbsp;&nbsp;X&nbsp;</button></center>';
     div.style.left='10px';
